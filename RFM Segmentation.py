7582a82ae50dea4553049aea62937e584f7b6242
Aishwarya Sarkar <81924922+Aishwarya-Sarkar@users.noreply.github.com>
2021-08-18 23:53:52 +0530

    Update RFM Segmentation.py

--- RFM Segmentation.py
+++ RFM Segmentation.py
@@ -1,1 +1,12 @@
+data_all= df.copy()
 
+#calculating total the total amount for each line item (unit price * Quantity)
+data_all['amount']= data_all['Quantity']* data_all['UnitPrice']
+data_all.head()
+
+#convert column to datetime
+data_all['InvoiceDate']=pd.to_datetime(data_all['InvoiceDate'])
+
+#setting date of analysis= 1 day after the most recent invoice
+analysis_date = pd.to_datetime(data['InvoiceDate'].max())+ dt.timedelta(days=1)
+data_all.head()

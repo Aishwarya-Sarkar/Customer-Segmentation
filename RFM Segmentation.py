8e3b5f028fc34139ad2e5593938272a8ab481bc9
Aishwarya Sarkar <81924922+Aishwarya-Sarkar@users.noreply.github.com>
2021-08-21 23:48:49 +0530

    Update RFM Segmentation.py

--- RFM Segmentation.py
+++ RFM Segmentation.py
@@ -11,3 +11,14 @@ data_all['InvoiceDate']=pd.to_datetime(data_all['InvoiceDate'])
 #setting date of analysis= 1 day after the most recent invoice
 analysis_date = pd.to_datetime(data['InvoiceDate'].max())+ dt.timedelta(days=1)
 data_all.head()
+
+#grouped = data_all.groupby(['CustomerID'])['InvoiceDate'].apply(lambda x: (analysis_date - x.max()).days).reset_index()
+grouped = data_all.groupby(['CustomerID'])\
+                .agg({'InvoiceDate': lambda x: (analysis_date - x.max()).days,
+                      'InvoiceNo': 'count',
+                      'amount': 'sum'})\
+                
+grouped.rename(columns = {'InvoiceDate': 'R_val',
+                                   'InvoiceNo': 'F_val',
+                                   'amount': 'M_val'}, inplace=True)
+grouped.head()
